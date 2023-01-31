9c177e1ca33f4910e7c1bbd888f0e5e293792b53
MentorSP <alexandersedykh@internet.ru>
2023-01-31 14:45:53 +0300

    Initial commit

---
+++ service/src/main/java/com/dmdev/service/AppRunner.java
@@ -0,0 +1,16 @@
+package com.dmdev.service;
+
+import com.dmdev.service.service.UserService;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppRunner {
+    public static void main( String[] args ) {
+
+        var userService = new UserService();
+
+        System.out.println(userService.getUser(25L));
+    }
+}
